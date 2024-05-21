244c7dda2ead63a2107a8a844fb6c6658028064c
weynard02 <samsicoweynard@gmail.com>
2024-05-21 21:07:47 +0700

    feat: Login and SignUp Blank page

--- app/src/main/java/com/weynard02/wisyuk/view/LoginActivity.kt
+++ app/src/main/java/com/weynard02/wisyuk/view/LoginActivity.kt
@@ -1,16 +1,17 @@
-package com.weynard02.wisyuk
+package com.weynard02.wisyuk.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.weynard02.wisyuk.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
