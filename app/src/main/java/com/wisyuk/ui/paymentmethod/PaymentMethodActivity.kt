ef0ee4deb1a5d10cbe73a4d10f5083bfce163f4c
Frederick Hidayat <frederickhidayat2003@gmail.com>
2024-06-02 20:44:52 +0700

    feat: payment method activity

---
+++ app/src/main/java/com/wisyuk/ui/paymentmethod/PaymentMethodActivity.kt
@@ -0,0 +1,21 @@
+package com.wisyuk.ui.paymentmethod
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.wisyuk.R
+
+class PaymentMethodActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_payment_method)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
