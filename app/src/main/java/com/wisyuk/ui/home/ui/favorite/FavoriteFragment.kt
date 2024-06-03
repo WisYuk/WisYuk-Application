211eda1bcbbb5a6c31954ea076ef6075edd5c694
weynard02 <samsicoweynard@gmail.com>
2024-06-03 07:52:56 +0700

    fix: bug fixes

--- app/src/main/java/com/wisyuk/ui/home/ui/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/wisyuk/ui/home/ui/favorite/FavoriteFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.wisyuk.databinding.FragmentFavoriteBinding
@@ -28,10 +27,10 @@ class FavoriteFragment : Fragment() {
         _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        favoriteViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textNotifications
+//        favoriteViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
--- app/src/main/java/com/wisyuk/ui/home/ui/plans/PlanFragment.kt
+++ app/src/main/java/com/wisyuk/ui/home/ui/plans/PlanFragment.kt
@@ -28,10 +28,10 @@ class PlanFragment : Fragment() {
         _binding = FragmentPlanBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        planViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textDashboard
+//        planViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
