f05b4b37e1a6968910a4509ebf3d233136ba410a
weynard02 <samsicoweynard@gmail.com>
2024-06-05 21:23:56 +0700

    feat: Signup, Login, and ViewProfile APIService

---
+++ app/src/main/java/com/wisyuk/data/response/LoginResponse.kt
@@ -0,0 +1,42 @@
+package com.wisyuk.data.response
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginResponse(
+
+	@field:SerializedName("message")
+	val message: String,
+
+	@field:SerializedName("user")
+	val user: User? = null,
+
+	@field:SerializedName("status")
+	val status: String
+)
+
+data class User(
+
+	@field:SerializedName("image")
+	val image: String? = null,
+
+	@field:SerializedName("password")
+	val password: String? = null,
+
+	@field:SerializedName("updated_at")
+	val updatedAt: String? = null,
+
+	@field:SerializedName("name")
+	val name: String? = null,
+
+	@field:SerializedName("bool_promotion")
+	val boolPromotion: Int? = null,
+
+	@field:SerializedName("created_at")
+	val createdAt: String? = null,
+
+	@field:SerializedName("id")
+	val id: Int? = null,
+
+	@field:SerializedName("email")
+	val email: String? = null
+)
